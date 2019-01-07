76b359cb5f9aefef1c8ac1ca9009a4e6f497389e
nikenoctaa <nikenlarasati765@gmail.com>
2019-01-07 13:57:32 +0700

    fix

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
         </div>
       </li>
       <li class="nav-item">
-            <a class="nav-link" href="admin/index.php" class="page-scroll">Admin</a>
+            <a class="nav-link" href="admin/index.php" class="page-scroll"></a>
       </li>
         </ul>
       </div>    
